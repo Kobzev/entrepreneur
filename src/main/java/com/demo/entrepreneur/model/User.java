fdad1fd05ce5387720cba5aac9e166f7b17f23e3
VladRybkin <RybkinVladyslav@gmail.com>
2019-09-13 20:58:24 +0300

    Create table branch (#1)
    * adder entity
    
    * adder entity
    
    * Made DTO from entity User
    
    * made id Long and BIGINT
    
    * made datasource.url localhost
    
    * Made dto and entity user, added constraints to email and login columns
    
    * fixed package names and spaces
    
    * added builder annotation
    
    * added password field to UserDTO
    
    * added entity annotations, fixed application.properties, fixed space in creat_tables-changelog.xml

---
+++ src/main/java/com/demo/entrepreneur/model/User.java
@@ -0,0 +1,33 @@
+package com.demo.entrepreneur.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+
+
+}
