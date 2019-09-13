cc5d486acce6cb38981281c6fc8cc12c89c95245
RybkinVladyslav <Footballer0@mail.ru>
2019-09-13 16:34:10 +0300

    added entity annotations, fixed application.properties, fixed space in creat_tables-changelog.xml

--- src/main/java/com/demo/entrepreneur/model/User.java
+++ src/main/java/com/demo/entrepreneur/model/User.java
@@ -1,20 +1,33 @@
 package com.demo.entrepreneur.model;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import javax.persistence.*;
 
 
+@Data
+@AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Entity
+@Table(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "login")
     private String login;
+    @Column(name = "password")
     private String password;
+    @Column(name = "email")
     private String email;
+    @Column(name = "firstname")
     private String firstName;
+    @Column(name = "lastname")
     private String lastName;
+
 
 }
