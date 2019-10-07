a9969e519f5f0d7bce807b6d996b9789110e1065
Zephyrkina <alma001@ukr.net>
2019-10-07 14:20:16 +0300

    add: email validation for allowed domain, email sending during user registration, update tests for email sending and validation service

--- src/main/java/com/demo/entrepreneur/service/impl/DefaultUserService.java
+++ src/main/java/com/demo/entrepreneur/service/impl/DefaultUserService.java
@@ -4,6 +4,8 @@ import com.demo.entrepreneur.dao.UserRepository;
 import com.demo.entrepreneur.dto.UserDto;
 import com.demo.entrepreneur.entity.User;
 import com.demo.entrepreneur.mapping.populator.impl.UserPopulator;
+import com.demo.entrepreneur.service.EmailSenderService;
+import com.demo.entrepreneur.service.EmailValidatorService;
 import com.demo.entrepreneur.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,9 +23,19 @@ public class DefaultUserService implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private EmailValidatorService emailValidator;
+
+    @Autowired
+    private EmailSenderService emailSender;
+
     @Override
     public User registerNewUser(UserDto userDto) {
+        if (!emailValidator.isValid(userDto.getEmail())) {
+            throw new IllegalArgumentException("User email is not valid");
+        }
         final User user = userPopulator.populateDataToEntity(userDto, new User());
+        emailSender.sendVerificationEmail(user.getEmail());
         return userRepository.save(user);
     }
 
