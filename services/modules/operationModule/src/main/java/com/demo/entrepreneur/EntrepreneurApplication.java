559bf22a212d03d6b808b8dbb383cfbb9b4f9d8c
Kostiantyn Kobziev <kostya.kobzev@gmail.com>
2019-12-28 20:34:32 +0200

    Global refactoring (#18)
    * add modules
    
    * extract currency service
    
    * adding module structure
    
    * fix build
    
    * temporally remove pom-inheritance.
    
    Co-authored-by: Kostiantyn Kobziev <39281230+kkobziev-hw@users.noreply.github.com>

---
+++ services/modules/operationModule/src/main/java/com/demo/entrepreneur/EntrepreneurApplication.java
@@ -0,0 +1,12 @@
+package com.demo.entrepreneur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrepreneurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntrepreneurApplication.class, args);
+    }
+}
